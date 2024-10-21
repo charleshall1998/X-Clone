d170bdea0aa75529441806ff8dcfe0be82f8fc3f
charleshall1998 <charleskhanhhall@gmail.com>
2024-10-21 19:46:56 -0400

    Added some code for header / footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from "./shared/layout/nav-bar/nav-bar.component";
+import { FooterComponent } from "./shared/layout/footer/footer.component";
+import { PostListComponent } from "./features/post-list/post-list.component";
 
 @NgModule({
   declarations: [
@@ -10,8 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    NavBarComponent,
+    FooterComponent,
+    PostListComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
