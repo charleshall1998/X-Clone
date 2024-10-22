6fc0d7198d564c36af0b2d43de13cff52081e54b
charleshall1998 <charleskhanhhall@gmail.com>
2024-10-22 17:13:20 -0400

    Added interpolation & profile picture to post.

--- src/app/features/post/post.component.ts
+++ src/app/features/post/post.component.ts
@@ -15,5 +15,9 @@ import { MatIcon } from '@angular/material/icon';
   styleUrl: './post.component.css'
 })
 export class PostComponent {
-  
+    
+  public userName: string = "Charles Hall";
+  public userProfilePictureSrc: string = "";
+  public content: string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
 }
