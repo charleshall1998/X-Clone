96f0d0cda1faf01659cac8395071a18b2f09c966
charleshall1998 <charleskhanhhall@gmail.com>
2024-10-22 18:34:30 -0400

    Added home & user sign up / login components. Added login / sign up button to nav bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { FooterComponent } from './shared/layout/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { PostListComponent } from "./features/post-list/post-list.component";
+import { HomeComponent } from "./features/home/home.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [NavBarComponent, FooterComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive, PostListComponent]
+  imports: [NavBarComponent, FooterComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive, PostListComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'x-clone';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router";
 import { ProfileComponent } from "./features/profile/profile.component";
 import { SettingsComponent } from "./features/settings/settings.component";
+import { HomeComponent } from "./features/home/home.component";
 
 export const routes: Routes = [
+    {
+        path: "home",
+        component: HomeComponent
+    },
     {
         path: "profile",
         component: ProfileComponent
