8846168caf012887e7ebf8ab7a5c88038f5482a4
charleshall1998 <charleskhanhhall@gmail.com>
2024-10-22 14:56:05 -0400

    Added post list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { NavBarComponent } from './shared/layout/nav-bar/nav-bar.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { PostListComponent } from "./features/post-list/post-list.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [NavBarComponent, FooterComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive]
+  imports: [NavBarComponent, FooterComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive, PostListComponent]
 })
 export class AppComponent {
   title = 'x-clone';
--- src/app/features/post-list/post-list.component.ts
+++ src/app/features/post-list/post-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { PostComponent } from "../post/post.component";
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [PostComponent, NgForOf],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
-
+  public posts = ["1", "2", "3"];
 }
