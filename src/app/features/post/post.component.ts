a1a4351010e3379fa09b084f808292b6c840c878
charleshall1998 <charleskhanhhall@gmail.com>
2024-10-22 16:42:50 -0400

    Created basic post layout. Resolved issue w/ autocomplete not finding Angular Material modules.

--- src/app/features/post/post.component.ts
+++ src/app/features/post/post.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [ MatCardModule, MatButtonModule, MatIcon ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
