bfdc1f8e31bcff4a5b28391a942dcf7fd78ec689
charleshall1998 <charleskhanhhall@gmail.com>
2024-10-21 20:29:39 -0400

    Added settings component & routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { ProfileComponent } from "./features/profile/profile.component";
+import { SettingsComponent } from "./features/settings/settings.component";
 
 export const routes: Routes = [
     {
         path: "profile",
         component: ProfileComponent
+    },
+    {
+        path: "settings",
+        component: SettingsComponent
     }
 ]
